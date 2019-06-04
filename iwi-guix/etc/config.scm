127823170bfbaf590aafa363038cba2001aab9bc
Oleg Bocharov <oleg.bocharov@ipec.co.uk>
2019-06-05 00:48:53 +0100

    [N] Init new configuration method for OS Guix

---
+++ iwi-guix/etc/config.scm
@@ -0,0 +1,8 @@
+;; File      : config.scm
+;; Path      : /etc/config.scm
+;; Author    : Sharlatan <sharlatanus@gmail.com>
+;; Created   : <2019-6-02 Sun 11:08:17 BST>
+;; Modified  : <2019-6-02 Sun 11:08:47 BST> Sharlatan
+;; Synopsis  : <Global configuration for Guix.>
+
+;; End of config.scm
